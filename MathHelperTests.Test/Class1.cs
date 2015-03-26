61d3e6b523a5e06952ba920b8be4efa148d045b9
jloleysens <jloleysens@gmail.com>
2015-03-26 15:10:25 +0200

    Initial commit
    Signed-off-by: jloleysens <jloleysens@gmail.com>

---
+++ MathHelperTests.Test/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MathHelperTests.Test
+{
+    public class TestClass
+    {
+		[TestCase]
+		public void AddTest()
+		{
+			ConsoleApplication1.MathsHelper helper = new ConsoleApplication1.MathsHelper();
+			int result = helper.Add(20, 10);
+			Assert.AreEqual(30, result);
+		}
+
+		[TestCase]
+		public void SubtractTest()
+		{
+			ConsoleApplication1.MathsHelper helper = new ConsoleApplication1.MathsHelper();
+			int result = helper.Subtract(20, 10);
+			Assert.AreEqual(10, result);
+		}
+    }
+}
